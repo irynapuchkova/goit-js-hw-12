2ce87f64b5ec1dccff441c08b824a3c5187ff4af
Iryna Puchkova <irynapuchkova18@gmail.com>
2021-07-22 11:36:13 +0300

    fix rendering

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import '../node_modules/notiflix-react/dist/notiflix-react-1.4.0.css'
 import Notiflix from 'notiflix-react/dist/notiflix-react-1.4.0'
 import { debounce } from 'lodash'
 
-import API from './js/fetchCountries'
+import {fetchCountries} from './js/fetchCountries'
 import getRefs from './js/get_refs'
 import countriesListTpl from './templates/countries_list.hbs'
 import countryCardTpl from './templates/country_info.hbs' 
@@ -17,28 +17,27 @@ const refs = getRefs();
 refs.input.addEventListener('input', debounce(handleInputInfo, DEBOUNCE_DELAY))
 
 function handleInputInfo(e) {
-  const inputContex = e.target.value.toLowerCase();
+  const inputContex = e.target.value.toLowerCase().trim();
+  console.log(inputContex.trim());
 
-  API.fetchCountries(inputContex)
-    .then(notifyInfoTooManyCountries)
-    .then(renderCountryList)
-    .then(renderCountryCard)
+  
+  if (inputContex === '') {
+    refs.countryInfo.innerHTML = ' ';
+    return;
+  } else {
+    fetchCountries(inputContex)
+    .then(renderQueryOfCountries)
     .catch(notifyNoCountry)
+  }
 }
 
-
-function renderCountryList(countries) {
-
+function renderQueryOfCountries(countries) {
   refs.countryInfo.innerHTML = ' ';
 
   if (countries.length > 1 && countries.length <= 10) {
-    const markup = countriesListTpl(countries);
-    refs.countryList.insertAdjacentHTML('beforeend', markup)
+      const markup = countriesListTpl(countries);
+      refs.countryList.insertAdjacentHTML('beforeend', markup)
   }
-  return countries;
-}
-
-function renderCountryCard(countries) {
 
   if (countries.length === 1) {
     refs.countryList.innerHTML = ' ';
@@ -48,20 +47,17 @@ function renderCountryCard(countries) {
     refs.countryInfo.innerHTML = markupCard;
     refs.countryInfo.insertAdjacentHTML('beforeend', `<p><span class="country-info__subtitle">Languages</span>: ${languages}</p>`)
   }
-}
-
-function notifyInfoTooManyCountries(countries) {
-
-  refs.countryList.innerHTML = ' ';
 
   if (countries.length > 10) {
+    refs.countryList.innerHTML = ' ';
     Notiflix.Notify.Info('Too many matches found. Please enter a more specific name.')  
   }
-  return countries;
+
 }
 
 function notifyNoCountry(error, countries) {
-  if ([] || !countries) {
+
+  if ([] || !countries || inputContex != ''.trim()) {
     console.log(error);
     Notiflix.Notify.Failure('Oops, there is no country with that name');
     refs.countryList.innerHTML = ' ';
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,8 @@
-export default {fetchCountries};
 
 const BASE_URL = 'https://restcountries.eu/rest/v2';
 const OPTIONS = `name;capital;flag;population;languages`;
 
-export function fetchCountries(name) {
+export  function fetchCountries(name) {
 
   return fetch(`${BASE_URL}/name/${name}?fields=${OPTIONS}`)
     .then(response => {
